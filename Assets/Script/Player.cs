4e51c06097bd3cf9b932680851a3ac426dccaacd
panchoo-k <panchoo_k@silpakorn.edu>
2026-08-01 15:40:05 +0700

    UI

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,44 @@
+using JetBrains.Rider.Unity.Editor;
+using System;
+using Unity.IO.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float forcePower;
+
+    [SerializeField]
+    private Rigidbody rb;
+
+    private InputAction moveAction;
+    private Vector2 moveValue;
+
+    [SerializeField]
+    private int point;
+    public int Point {  get { return point; } set { point = value; } }
+
+    [SerializeField]
+    private int hp;
+    public int HP { get { return hp; } set { hp = value; } }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveLeftorRight();
+    }
+
+    private void MoveLeftorRight()
+    {
+        moveValue = moveAction.ReadValue<Vector2>();
+        rb.AddForce(moveValue.x * Vector3.right * forcePower);
+    }
+}
